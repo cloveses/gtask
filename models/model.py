8cad9b6fdf5d5356729b171db65b1ef298f1c043
cloveses <cloveses@163.com>
2018-09-27 16:47:46 +0800

    m

--- models/model.py
+++ models/model.py
@@ -5,8 +5,44 @@ from pony.orm import *
 db = Database()
 
 class User(db.Entity):
-    name = Optional(str)
+    name = Required(str)
     passwd = Required(str)
+
+class Role(db.Entity):
+    name = Required(str)
+
+class Product(db.Entity):
+    name = Required(str)
+    common_name = Optional(str)
+    price = Required(float)
+    requirement = Required(str)
+    certification = Required(str)
+    category = Optional('Category')
+    producer = Required('Producer')
+    supplier = Required('Supplier')
+    entry_record = Set('EntryRecord')
+
+class Producer(db.Entity):
+    name = Required(str)
+    addr = Required(str)
+    telephoe = Required(str)
+    products = Set(Product)
+
+class Supplier(db.Entity):
+    name = Required(str)
+    addr = Required(str)
+    telephoe = Required(str)
+    product = Set(Product)
+
+class Category(db.Entity):
+    name = Required(str)
+    products = Set(Product)
+
+class EntryRecord(db.Entity):
+    quantity = Required(float)
+    date = Required(datetime.datetime)
+    term_validity = Required(datetime.datetime)
+    product = Required(Product)
 
 set_sql_debug(True)
 filename = os.path.join(os.path.abspath(os.curdir),'my.db')
